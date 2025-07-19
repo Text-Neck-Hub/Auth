d40fbb3d2c646652326549f38001abe14803ab81
creepereye1204 <iostream1204@gmail.com>
2025-07-19 08:09:56 +0000

    Refactoring v4

--- authentication/apps.py
+++ authentication/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authentication'
+
+    def ready(self):
+        import authentication.signals.user_profile_signal
